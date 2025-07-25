8e4cdc63defe4db2918880912bb6c3847fe7f335
Allison <allisong4@users.noreply.github.com>
2025-07-25 11:20:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, Query
-from fastapi.responses import StreamingResponse
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Query, Response
+from fastapi.responses import StreamingResponse, JSONResponse
 import httpx
 
 app = FastAPI()
@@ -9,14 +8,19 @@ app = FastAPI()
 async def proxy(url: str = Query(...)):
     try:
         async with httpx.AsyncClient() as client:
-            remote = await client.get(url, stream=True)
-            headers = {
-                "Content-Type": remote.headers.get("content-type", "application/octet-stream"),
-                "Access-Control-Allow-Origin": "*"
-            }
-            return StreamingResponse(remote.aiter_bytes(), headers=headers, status_code=remote.status_code)
+            async with client.stream("GET", url) as remote:
+                if remote.status_code >= 400:
+                    content = await remote.aread()
+                    return Response(content, status_code=remote.status_code, headers={
+                        "Content-Type": remote.headers.get("content-type", "application/octet-stream"),
+                        "Access-Control-Allow-Origin": "*"
+                    })
+                headers = {
+                    "Content-Type": remote.headers.get("content-type", "application/octet-stream"),
+                    "Access-Control-Allow-Origin": "*"
+                }
+                return StreamingResponse(remote.aiter_bytes(), headers=headers, status_code=remote.status_code)
     except Exception as e:
-        # Always return CORS header even on error!
         return JSONResponse(
             content={"error": str(e)},
             status_code=500,
